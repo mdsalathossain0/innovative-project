f482fd537029ce19ff5ea90770d4dc851ef52926
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-03-16 12:42:44 +0600

    navber done

---
+++ src/layouts/Nav.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import Container from '../components/Container'
+import Flex from '../components/Flex'
+import Image from '../components/Images'
+import List from '../components/List'
+import Button from '../components/Button'
+import Logo from '../assets/mainlogo.png'
+
+const Nav = () => {
+  return (
+    <div>
+        <nav className='bg-[#160C6D] py-5'>
+           <Container>
+            <Flex className='items-center'>
+                <div className='w-2/12'>
+                    <Image src={Logo} alt={'Logo'}/>
+                </div>
+                <div className='w-8/12'>
+                    <ul className='flex gap-x-10 justify-center cursor-pointer'>
+                    <List text='Home'/>
+                    <List text='About'/>
+                    <List text='Service'/>
+                    <List text='Portfolio'/>
+                    <List text='Price'/>
+                    <List text='Blog'/>
+                    </ul>
+                </div>
+                <div className='w-2/12 text-right'>
+                    <Button text='Contact Us'/>
+                </div>
+            </Flex>
+           </Container>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
